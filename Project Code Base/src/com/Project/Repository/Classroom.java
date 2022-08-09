85b8e8a7ce4b9bb3fa9e9c9dcf02c261744b0dce
Tabassum Bari <108945765+TabassumBari@users.noreply.github.com>
2022-08-09 12:01:13 +0600

    Update Classroom.java

--- Project Code Base/src/com/Project/Repository/Classroom.java
+++ Project Code Base/src/com/Project/Repository/Classroom.java
@@ -1,5 +1,42 @@
 package com.Project.Repository;
 
-public class Classroom {
+import java.util.*;
 
+import com.Project.Entities.Student;
+import com.Project.Entities.Teacher;
+public class Classroom {
+private int numberofStudents;
+private int numberofTeachers;
+private ArrayList<Student> students=new ArrayList<Student>();
+private ArrayList<Teacher> teachers=new ArrayList<Teacher>();
+public int getNumberofStudents() {
+	return numberofStudents;
 }
+
+public void setNumberofStudents(int numberofStudents) {
+	this.numberofStudents = numberofStudents;
+}
+
+public int getNumberofTeachers() {
+	return numberofTeachers;
+}
+
+public void setNumberofTeachers(int numberofTeachers) {
+	this.numberofTeachers = numberofTeachers;
+}
+
+public ArrayList<Student> getStudents() {
+	return students;
+}
+
+public void setStudents(ArrayList<Student> students) {
+	this.students = students;
+}
+
+public ArrayList<Teacher> getTeachers() {
+	return teachers;
+}
+
+public void setTeachers(ArrayList<Teacher> teachers) {
+	this.teachers = teachers;
+}}
