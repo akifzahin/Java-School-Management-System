6c537d51fd174160c80117fbfce46ab9799afb50
Mohammed Aman Bhuiyan <mohammed.aman@northsouth.edu>
2022-08-15 18:01:18 +0600

    Update WelcomeUI.java

--- Project Code Base/src/com/Project/UI/WelcomeUI.java
+++ Project Code Base/src/com/Project/UI/WelcomeUI.java
@@ -27,12 +27,10 @@ public class WelcomeUI extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+	
 	public WelcomeUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(101, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
