e8014a9c91e4c9f0ae30fb6a2866b68c2dbfea5d
TabassumBari <tabassum.bari@northsouth.edu>
2022-08-18 22:54:44 +0600

    Created Registration Forms

--- Project Code Base/src/com/Project/UI/StudentFormUI.java
+++ Project Code Base/src/com/Project/UI/StudentFormUI.java
@@ -6,16 +6,23 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JRadioButton;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class StudentFormUI extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
 
 	/**
 	 * Launch the application.
@@ -37,45 +44,105 @@ public class StudentFormUI extends JFrame {
 	 * Create the frame.
 	 */
 	public StudentFormUI() {
-		setBackground(new Color(204, 255, 255));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1057, 690);
+		setBounds(100, 100, 429, 529);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(204, 255, 255));
+		contentPane.setBackground(new Color(135, 206, 250));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel_2 = new JLabel("Student Info Form");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 30));
-		lblNewLabel_2.setBounds(324, 11, 477, 72);
+		JLabel lblNewLabel = new JLabel("STUDENT ");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel.setBounds(48, 11, 345, 27);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(230, 49, 148, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("User Name");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_1.setBounds(28, 49, 68, 20);
+		contentPane.add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(230, 81, 148, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("First Name");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_2.setBounds(28, 83, 68, 17);
 		contentPane.add(lblNewLabel_2);
 		
-		JButton btnNewButton = new JButton("Submit");
-		btnNewButton.setBounds(328, 542, 89, 36);
+		textField_2 = new JTextField();
+		textField_2.setBounds(230, 111, 148, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Last Name");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_3.setBounds(28, 114, 68, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(230, 142, 148, 20);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("Password");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_4.setBounds(28, 139, 68, 23);
+		contentPane.add(lblNewLabel_4);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(230, 171, 148, 20);
+		contentPane.add(textField_4);
+		textField_4.setColumns(10);
+		
+		JLabel lblNewLabel_5 = new JLabel("Confirm Password");
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_5.setBounds(28, 176, 123, 17);
+		contentPane.add(lblNewLabel_5);
+		
+		textField_5 = new JTextField();
+		textField_5.setBounds(230, 208, 148, 20);
+		contentPane.add(textField_5);
+		textField_5.setColumns(10);
+		
+		JLabel lblNewLabel_6 = new JLabel("Email");
+		lblNewLabel_6.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_6.setBounds(28, 211, 92, 14);
+		contentPane.add(lblNewLabel_6);
+		
+		JLabel lblNewLabel_7 = new JLabel("Gender");
+		lblNewLabel_7.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_7.setBounds(28, 256, 68, 14);
+		contentPane.add(lblNewLabel_7);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Male");
+		rdbtnNewRadioButton.setBackground(new Color(135, 206, 250));
+		rdbtnNewRadioButton.setBounds(243, 252, 68, 23);
+		contentPane.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Female");
+		rdbtnNewRadioButton_1.setBackground(new Color(135, 206, 250));
+		rdbtnNewRadioButton_1.setBounds(313, 252, 65, 23);
+		contentPane.add(rdbtnNewRadioButton_1);
+		
+		JButton btnNewButton = new JButton("Register");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnNewButton.setBounds(260, 315, 118, 23);
 		contentPane.add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("Back");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-				CreateAccountUI frame = new CreateAccountUI();
-				frame.setVisible(true);
-			}
-		});
-		btnNewButton_1.setBounds(460, 542, 89, 36);
-		contentPane.add(btnNewButton_1);
-		
-		JButton btnNewButton_1_1 = new JButton("Home");
-		btnNewButton_1_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-				WelcomeUI frame = new WelcomeUI();
-				frame.setVisible(true);
-			}
-		});
-		btnNewButton_1_1.setBounds(590, 542, 89, 36);
-		contentPane.add(btnNewButton_1_1);
+		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("I accept all  terms and Conditions");
+		rdbtnNewRadioButton_2.setBackground(new Color(135, 206, 250));
+		rdbtnNewRadioButton_2.setSelected(true);
+		rdbtnNewRadioButton_2.setBounds(28, 315, 200, 21);
+		contentPane.add(rdbtnNewRadioButton_2);
 	}
-
 }
--- Project Code Base/src/com/Project/UI/teacherFormUI.java
+++ Project Code Base/src/com/Project/UI/teacherFormUI.java
@@ -1,19 +1,41 @@
 package com.Project.UI;
 
+
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.Color;
-import javax.swing.JLabel;
+import javax.swing.JTextField;
 import java.awt.Font;
+import javax.swing.SwingConstants;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import java.awt.Color;
 
 public class teacherFormUI extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JTextField textField_1;
+	private JLabel lblNewLabel_2;
+	private JTextField textField_2;
+	private JLabel lblNewLabel_3;
+	private JTextField textField_3;
+	private JLabel lblNewLabel_4;
+	private JTextField textField_4;
+	private JLabel lblNewLabel_5;
+	private JTextField textField_5;
+	private JLabel lblNewLabel_6;
+	private JRadioButton rdbtnNewRadioButton;
+	private JRadioButton rdbtnNewRadioButton_1;
+	private JLabel lblNewLabel_7;
+	private JRadioButton rdbtnNewRadioButton_2;
+	private JButton btnNewButton;
 
 	/**
 	 * Launch the application.
@@ -35,30 +57,105 @@ public class teacherFormUI extends JFrame {
 	 * Create the frame.
 	 */
 	public teacherFormUI() {
+		setForeground(Color.CYAN);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1059, 679);
+		setBounds(100, 100, 587, 482);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(204, 255, 255));
+		contentPane.setBackground(new Color(135, 206, 250));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel_2 = new JLabel("Teacher Info Form");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 30));
-		lblNewLabel_2.setBounds(295, 11, 477, 72);
+		textField = new JTextField();
+		textField.setBounds(336, 80, 196, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		lblNewLabel = new JLabel("User");
+		lblNewLabel.setBounds(206, 83, 49, 14);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
+		contentPane.add(lblNewLabel);
+		
+		lblNewLabel_1 = new JLabel("Teacher");
+		lblNewLabel_1.setBounds(206, 0, 386, 25);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(336, 126, 196, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		lblNewLabel_2 = new JLabel("First Name");
+		lblNewLabel_2.setBounds(206, 128, 78, 17);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 11));
 		contentPane.add(lblNewLabel_2);
 		
-		JButton btnNewButton = new JButton("Submit");
-		btnNewButton.setBounds(295, 550, 89, 36);
+		textField_2 = new JTextField();
+		textField_2.setBounds(336, 170, 196, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		lblNewLabel_3 = new JLabel("Last Name");
+		lblNewLabel_3.setBounds(206, 173, 64, 14);
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 11));
+		contentPane.add(lblNewLabel_3);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(336, 219, 196, 20);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+		
+		lblNewLabel_4 = new JLabel("Password");
+		lblNewLabel_4.setBounds(206, 222, 78, 14);
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 11));
+		contentPane.add(lblNewLabel_4);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(336, 263, 196, 20);
+		contentPane.add(textField_4);
+		textField_4.setColumns(10);
+		
+		lblNewLabel_5 = new JLabel("Confirm Password");
+		lblNewLabel_5.setBounds(206, 257, 104, 33);
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 11));
+		contentPane.add(lblNewLabel_5);
+		
+		textField_5 = new JTextField();
+		textField_5.setBounds(336, 302, 196, 20);
+		contentPane.add(textField_5);
+		textField_5.setColumns(10);
+		
+		lblNewLabel_6 = new JLabel("Email");
+		lblNewLabel_6.setBounds(206, 301, 90, 25);
+		lblNewLabel_6.setFont(new Font("Tahoma", Font.BOLD, 11));
+		contentPane.add(lblNewLabel_6);
+		
+		rdbtnNewRadioButton = new JRadioButton("Male");
+		rdbtnNewRadioButton.setBounds(336, 333, 90, 23);
+		rdbtnNewRadioButton.setBackground(new Color(135, 206, 250));
+		contentPane.add(rdbtnNewRadioButton);
+		
+		rdbtnNewRadioButton_1 = new JRadioButton("Female");
+		rdbtnNewRadioButton_1.setBounds(439, 333, 93, 23);
+		rdbtnNewRadioButton_1.setBackground(new Color(135, 206, 250));
+		contentPane.add(rdbtnNewRadioButton_1);
+		
+		lblNewLabel_7 = new JLabel("Gender");
+		lblNewLabel_7.setBounds(206, 337, 49, 14);
+		lblNewLabel_7.setFont(new Font("Tahoma", Font.BOLD, 11));
+		contentPane.add(lblNewLabel_7);
+		
+		rdbtnNewRadioButton_2 = new JRadioButton("I Accept all terms and Conditions");
+		rdbtnNewRadioButton_2.setBounds(204, 372, 202, 23);
+		rdbtnNewRadioButton_2.setBackground(new Color(135, 206, 250));
+		contentPane.add(rdbtnNewRadioButton_2);
+		
+		btnNewButton = new JButton("Register");
+		btnNewButton.setBounds(443, 372, 89, 23);
+		btnNewButton.setBackground(new Color(0, 0, 255));
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 11));
 		contentPane.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Back");
-		btnNewButton_1.setBounds(470, 550, 89, 36);
-		contentPane.add(btnNewButton_1);
-		
-		JButton btnNewButton_1_1 = new JButton("Home");
-		btnNewButton_1_1.setBounds(632, 550, 89, 36);
-		contentPane.add(btnNewButton_1_1);
 	}
-
 }
