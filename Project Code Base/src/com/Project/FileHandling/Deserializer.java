9b8678e5bb3c5e0961188cb42dc4c3a2a050ad5c
akifzahin <zahinakif25@gmail.com>
2022-08-24 05:22:31 +0600

    Created File Handling Classes
    Respective methods were created to store the object and its current state in a .ser file

---
+++ Project Code Base/src/com/Project/FileHandling/Deserializer.java
@@ -0,0 +1,72 @@
+package com.Project.FileHandling;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import com.Project.Entities.Student;
+import com.Project.Entities.Teacher;
+
+public class Deserializer {
+	public static Student deserialize(Student student)
+	{	
+		Student studentObject = null;
+		String name = student.getFullName();
+		try
+		{
+			String path = "src\\repository data\\studentData\\"+name+".ser";
+			FileInputStream fileInputStream = new FileInputStream(path);
+			ObjectInputStream objInputStream = new ObjectInputStream(fileInputStream);
+			
+			studentObject = (Student) objInputStream.readObject();
+			
+			objInputStream.close();
+			fileInputStream.close();
+			
+		}
+		catch(IOException E)
+		{
+			E.printStackTrace();
+
+		}
+		catch(ClassNotFoundException E)
+		{
+			System.out.println("Student class not found!");
+			E.printStackTrace();
+
+		}
+		return studentObject;
+		
+	}
+	
+	public static Teacher deserialize(Teacher teacher)
+	{	
+		Teacher teacherObject = null;
+		String name = teacher.getFullName();
+		try
+		{	
+			String path = "src\\repository data\\teacherData\\"+name+".ser";
+			FileInputStream fileInputStream = new FileInputStream(path);
+			ObjectInputStream objInputStream = new ObjectInputStream(fileInputStream);
+			
+			teacherObject = (Teacher) objInputStream.readObject();
+			
+			objInputStream.close();
+			fileInputStream.close();
+			
+		}
+		catch(IOException E)
+		{
+			E.printStackTrace();
+
+		}
+		catch(ClassNotFoundException E)
+		{
+			System.out.println("Teacher class not found!");
+			E.printStackTrace();
+
+		}
+		return teacherObject;
+		
+	}
+}
---
+++ Project Code Base/src/com/Project/FileHandling/Serializer.java
@@ -0,0 +1,57 @@
+package com.Project.FileHandling;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import com.Project.Entities.Student;
+import com.Project.Entities.Teacher;
+
+public class Serializer 
+{
+	public static void serialize(Student student)
+	{
+		String name = student.getFullName();
+		try
+		{
+			String path = "src\\repository data\\studentData\\"+name+".ser";
+			FileOutputStream fileOutputStream = new FileOutputStream(path);
+			ObjectOutputStream objOutputStream = new ObjectOutputStream(fileOutputStream);
+			
+			objOutputStream.writeObject(student);
+			objOutputStream.close();
+			
+			fileOutputStream.close();
+			
+		}
+		catch(IOException E)
+		{
+			E.printStackTrace();
+		}
+	}
+
+
+	public static void serialize(Teacher teacher)
+	{
+		String name = teacher.getFullName();
+		try
+		{
+			String path = "src\\repository data\\teacherData\\"+name+".ser";
+			FileOutputStream fileOutputStream = new FileOutputStream(path);
+			ObjectOutputStream objOutputStream = new ObjectOutputStream(fileOutputStream);
+			
+			objOutputStream.writeObject(teacher);
+			objOutputStream.close();
+			
+			fileOutputStream.close();
+			
+		}
+		catch(IOException E)
+		{
+			E.printStackTrace();
+		}
+		
+		System.out.println("Successfully Saved Teacher");
+	}
+
+}
