babf16d3e58c316e1a9e1978cfa735d6206ee5fd
akifzahin <zahinakif25@gmail.com>
2022-08-25 03:46:18 +0600

    Created Final Run

--- Project Code Base/src/com/Project/Testing/TestMain.java
+++ Project Code Base/src/com/Project/Testing/TestMain.java
@@ -1,5 +1,6 @@
 package com.Project.Testing;
 import com.Project.Repository.*;
+import com.Project.UI.WelcomeUI;
 import com.Project.Entities.*;
 import com.Project.FileHandling.Deserializer;
 import com.Project.FileHandling.Serializer;
@@ -9,15 +10,9 @@ public class TestMain {
 
 	public static void main(String[] args) 
 	{
-		String path = "src\\repository data\\studentData\\Akif.ser";
-		File file = new File(path);
+		WelcomeUI frame = new WelcomeUI();
+		frame.setVisible(true);
 		
-		System.out.println("" + Deserializer.deserializeStudentFile(file).printUser());
-		
-
-		
-		
-
 	}
 
 }
